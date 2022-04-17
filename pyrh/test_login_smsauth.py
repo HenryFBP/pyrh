488d813bd31c29751b66654c3a8ef04551ffac39
Henry Post <HenryFBP@gmail.com>
2022-04-17 12:15:22 -0400

    improve login test

--- pyrh/test_login_smsauth.py
+++ pyrh/test_login_smsauth.py
@@ -13,6 +13,12 @@ def get_robinhood_login_json(path='~/.robinhood/login.json') -> Dict:
 if __name__ == '__main__':
     creds = get_robinhood_login_json()
     rh = Robinhood(username=creds['email'], password=creds['password'], challenge_type='sms')
-    print("rh.oauth.is_valid={}".format(rh.oauth.is_valid))
     del creds
+
+    print("rh.oauth.is_valid={}".format(rh.oauth.is_valid))
+    if not rh.oauth.is_valid:
+        print("logging in again.")
+        rh.login()
+
+    print("AAPL price:")
     rh.print_quote("AAPL")
